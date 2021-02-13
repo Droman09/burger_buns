ee45094cb255583b358585f66a17d9985bbe2b5a
David Roman <david_roman09@yahoo.com>
2021-02-12 21:09:32 -0600

    included styles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false) 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', false) 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Single Burger', false) 
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Single Burger', false);
 
 
 SELECT * FROM burgers; 
